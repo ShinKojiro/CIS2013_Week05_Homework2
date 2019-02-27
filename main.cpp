fdcc2a72b7f2fa6f27e17d6697d09ca8fd2109cf
James Hall <42621442+ShinKojiro@users.noreply.github.com>
2019-02-27 14:10:47 -0600

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void countsInput{
+    
+}
+
+int main(){
+    int edocCount = 0;
+    int edocCandies = 0;
+    int margorpCount = 0;
+    int experiencePossible = 0;
+    bool notDone = true;
+    char cont = 'y';
+}
